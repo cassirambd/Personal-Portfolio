208afeb6336e6902e174136571f636d5c14287e2
cassirambd <cassiram15@gmail.com>
2022-11-22 23:26:37 -0300

    facilito el tuto

--- js/script.js
+++ js/script.js
@@ -16,21 +16,19 @@ var typed2 = new Typed(".typing-name", {
 /* ======================= Aside ======================= */
 const nav = document.querySelector(".nav"),
   navList = nav.querySelectorAll("li"),
-  totalNavList = navList.length;
-
-(allSection = document.querySelectorAll(".section")),
-  (totalSection = allSection.length);
+  totalNavList = navList.length,
+  allSection = document.querySelectorAll(".section"),
+  totalSection = allSection.length;
 
 for (let i = 0; i < totalNavList; i++) {
   const a = navList[i].querySelector("a");
 
   a.addEventListener("click", function () {
-    for (let i = 0; i < totalSection; i++) {
-      allSection[i].classList.remove("back-section");
-    }
+    removeBackSection();
     for (let j = 0; j < totalNavList; j++) {
       if (navList[j].querySelector("a").classList.contains("active")) {
-        allSection[j].classList.add("back-section");
+        addBackSection(j);
+        // allSection[j].classList.add("back-section");
       }
       navList[j].querySelector("a").classList.remove("active");
     }
@@ -40,6 +38,16 @@ for (let i = 0; i < totalNavList; i++) {
       asideSectionTogglerBtn();
     }
   });
+}
+
+function removeBackSection() {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("back-section");
+  }
+}
+
+function addBackSection(num) {
+  allSection[num].classList.add("back-section");
 }
 
 function showSection(element) {
@@ -54,12 +62,21 @@ function updateNav(element) {
   for (let i = 0; i < totalNavList; i++) {
     navList[i].querySelector("a").classList.remove("active");
     const target = element.getAttribute("href").split("#")[1];
+    if (
+      (target === navList[i].querySelector("a").getAttribute("href"),
+      split("#")[1])
+    ) {
+      navList[i].querySelector("a").classList.add("active");
+    }
   }
 }
 
 document.querySelector(".hire-me").addEventListener("click", function () {
+  const sectionIndex = this.getAttribute("data-section-index");
   showSection(this);
   updateNav(this);
+  removeBackSection();
+  addBackSection(sectionIndex);
 });
 
 const navTogglerBtn = document.querySelector(".nav-toggler"),
@@ -67,6 +84,7 @@ const navTogglerBtn = document.querySelector(".nav-toggler"),
 navTogglerBtn.addEventListener("click", () => {
   asideSectionTogglerBtn();
 });
+
 function asideSectionTogglerBtn() {
   aside.classList.toggle("open");
   navTogglerBtn.classList.toggle("open");
