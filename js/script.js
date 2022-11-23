47472e0391c009e62b4602f3692cfd0e9eae0366
cassirambd <cassiram15@gmail.com>
2022-11-22 22:05:32 -0300

    add slides

---
+++ js/script.js
@@ -0,0 +1,42 @@
+/* ======================= typing animation ======================= */
+var typed = new Typed(".typing", {
+  strings: ["", "Full Stack Developer", "Front End Developer"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+
+var typed2 = new Typed(".typing-name", {
+  strings: ["", "Barbara Cassiram"],
+  typeSpeed: 100,
+  BackSpeed: 60,
+  loop: true,
+});
+
+/* ======================= Aside ======================= */
+const nav = document.querySelector(".nav"),
+  navList = nav.querySelectorAll("li"),
+  totalNavList = navList.length;
+(allSection = document.querySelectorAll(".section")),
+  (totalSection = allSection.length);
+for (let i = 0; i < totalNavList; i++) {
+  const a = navList[i].querySelector("a");
+  a.addEventListener("click", function () {
+    for (let j = 0; j < totalNavList; j++) {
+      if (navList[j].querySelector("a").classList.contains("active")) {
+        allSection[j].classList.add("back-section");
+      }
+      navList[j].querySelector("a").classList.remove("active");
+    }
+    this.classList.add("active");
+    showSection(this);
+  });
+}
+
+function showSection(element) {
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.remove("active");
+  }
+  const target = element.getAttribute("href").split("#")[1];
+  document.querySelector("#" + target).classList.add("active");
+}
