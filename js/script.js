8349efb479dd31089cd1b2a3be0bc658600fa8ee
cassirambd <cassiram15@gmail.com>
2022-11-22 22:42:53 -0300

    changes

--- js/script.js
+++ js/script.js
@@ -17,11 +17,17 @@ var typed2 = new Typed(".typing-name", {
 const nav = document.querySelector(".nav"),
   navList = nav.querySelectorAll("li"),
   totalNavList = navList.length;
+
 (allSection = document.querySelectorAll(".section")),
   (totalSection = allSection.length);
+
 for (let i = 0; i < totalNavList; i++) {
   const a = navList[i].querySelector("a");
+
   a.addEventListener("click", function () {
+    for (let i = 0; i < totalSection; i++) {
+      allSection[i].classList.remove("back-section");
+    }
     for (let j = 0; j < totalNavList; j++) {
       if (navList[j].querySelector("a").classList.contains("active")) {
         allSection[j].classList.add("back-section");
@@ -30,6 +36,9 @@ for (let i = 0; i < totalNavList; i++) {
     }
     this.classList.add("active");
     showSection(this);
+    if (window.innerWidth < 1200) {
+      asideSectionTogglerBtn();
+    }
   });
 }
 
@@ -39,4 +48,29 @@ function showSection(element) {
   }
   const target = element.getAttribute("href").split("#")[1];
   document.querySelector("#" + target).classList.add("active");
+}
+
+function updateNav(element) {
+  for (let i = 0; i < totalNavList; i++) {
+    navList[i].querySelector("a").classList.remove("active");
+    const target = element.getAttribute("href").split("#")[1];
+  }
+}
+
+document.querySelector(".hire-me").addEventListener("click", function () {
+  showSection(this);
+  updateNav(this);
+});
+
+const navTogglerBtn = document.querySelector(".nav-toggler"),
+  aside = document.querySelector(".aside");
+navTogglerBtn.addEventListener("click", () => {
+  asideSectionTogglerBtn();
+});
+function asideSectionTogglerBtn() {
+  aside.classList.toggle("open");
+  navTogglerBtn.classList.toggle("open");
+  for (let i = 0; i < totalSection; i++) {
+    allSection[i].classList.toggle("open");
+  }
 }
